254ec2d1ee120a5a1e0816a159ff40139a53e1c6
AlliedToasters <toast.hypermeow@gmail.com>
2026-04-16 19:52:08 -0700

    human noises

---
+++ util.py
@@ -0,0 +1,10 @@
+from transformers import AutoTokenizer
+
+def safe_load_tokenizer(model_name):
+
+    if 'mistral' in model_name.lower():
+            tokenizer = AutoTokenizer.from_pretrained(model_name, fix_mistral_regex=True)
+    else:
+        tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+    return tokenizer
